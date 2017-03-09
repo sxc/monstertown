6c9dbb639dcbcceaa94ef2c150ab1e937aa7ec24
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-09 11:09:21 +0800

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Shen Xiaochun on 2017/3/9.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
